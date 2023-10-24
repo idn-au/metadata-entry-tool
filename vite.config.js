c4d870e35feb8093af786a90d61ed83d03a4e905
jamiefeiss <jamie.feiss@gmail.com>
2023-10-24 15:51:16 +1000

    Fixed vue-fontawesome build error, updated workflow for sass build error

--- vite.config.js
+++ vite.config.js
@@ -12,8 +12,5 @@ export default defineConfig({
             "@": fileURLToPath(new URL("./src", import.meta.url))
         }
     },
-    base: process.env.NODE_ENV === "production" ? "/idn-metadata-creator/" : "/",
-    define: {
-        global: {},
-    }
+    base: process.env.NODE_ENV === "production" ? "/idn-metadata-creator/" : "/"
 });
