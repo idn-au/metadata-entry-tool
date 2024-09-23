e179e156c68df0421cb00fb3713ef9bf551cc532
jamiefeiss <jamie.feiss@gmail.com>
2024-09-23 15:58:14 +1000

    Added a sentence to the home page referring to the AgentsDB

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,6 +10,7 @@ import MetadataForm from "@/components/MetadataForm.vue";
     <p>In order to complete a compliant metadata record for your data, you need to fill out the information required in the form below. You have the option of completing only the minimum required information (indicated by a red asterix) but it is recommended that you include as much information as possible in the form.</p>
     <p>The more information you are able to provide, the higher the <a href="https://force11.org/info/the-fair-data-principles" target="_blank" rel="noopener noreferrer">FAIR</a> and <a href="https://www.gida-global.org/care" target="_blank" rel="noopener noreferrer">CARE</a> scores for your metadata. For now, these scores generate placeholder values.</p>
     <p>The metadata in RDF format can optionally be viewed on the right of the form. The RDF and scores are updated as you complete the form. More information about the metadata profile can be found <a href="https://data.idnau.org/pid/cp/guide" target="_blank" rel="noopener noreferrer">here</a>.</p>
+    <p>This form uses the IDN's collection of people and organisations, or "agents", that are related to metadata and datasets in the IDN, called the <a href="https://agentsdb.idnau.org" target="_blank" rel="noopener noreferrer">Agents Database</a>. You can refer to this list of agents or create a custom agent if required.</p>
     <MetadataForm />
 </template>
 
